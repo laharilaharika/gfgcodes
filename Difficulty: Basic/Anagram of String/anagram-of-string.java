a6c06eaeceef6080715dfdf37ddf8782c2a45f54
laharilaharika <155244861+laharilaharika@users.noreply.github.com>
2025-06-04 13:44:05 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Anagram of String/anagram-of-string.java
+++ Difficulty: Basic/Anagram of String/anagram-of-string.java
@@ -1,25 +1,3 @@
-//{ Driver Code Starts
-// saksham raj seth
-import java.util.*;
-import java.util.stream.*;
-
-class Anagrams {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int t = sc.nextInt();
-        while (t-- > 0) {
-            String s = sc.next();
-            String s1 = sc.next();
-            GfG g = new GfG();
-            System.out.println(g.remAnagrams(s, s1));
-
-            System.out.println("~");
-        }
-    }
-}
-// } Driver Code Ends
-
-
 /*Complete the function below*/
 class GfG {
     public int remAnagrams(String s1, String s2) {
