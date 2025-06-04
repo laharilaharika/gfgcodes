48d4a856f9ff8541740d29cb048b6484a2ea3076
laharilaharika <155244861+laharilaharika@users.noreply.github.com>
2025-06-04 13:41:17 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Remove common characters and concatenate/remove-common-characters-and-concatenate.java
+++ Difficulty: Basic/Remove common characters and concatenate/remove-common-characters-and-concatenate.java
@@ -1,32 +1,10 @@
-//{ Driver Code Starts
-import java.util.*;
-import java.lang.*;
-import java.io.*;
-
-class GFG{
-    public static void main(String args[]){
-        Scanner in=new Scanner(System.in);
-        int t=in.nextInt();
-        
-        while(t--!=0){
-            String s1=in.next(),
-                   s2=in.next();
-            
-            Solution obj = new Solution();
-            
-            System.out.println(obj.concatenatedString(s1,s2));
-        
-System.out.println("~");
-}
-    }
-}
-
-// } Driver Code Ends
 
 
 class Solution {
+    // Function to remove common characters and concatenate two strings.
     public static String concatenatedString(String s1, String s2) {
-        int[] freq1 = new int[256];
+        // Your code here
+         int[] freq1 = new int[256];
         int[] freq2 = new int[256];
 
         for (char ch : s1.toCharArray()) {
