e9635571a441f2f6bf38c57b21cfafedeb0dcd00
laharilaharika <155244861+laharilaharika@users.noreply.github.com>
2025-06-04 13:52:46 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Anagram Palindrome/anagram-palindrome.java
+++ Difficulty: Basic/Anagram Palindrome/anagram-palindrome.java
@@ -1,37 +1,7 @@
-//{ Driver Code Starts
-import java.util.*;
-import java.lang.*;
-import java.io.*;
-
-class GFG
-{
-    public static void main (String[] args)
-    {
-		Scanner in = new Scanner(System.in);
-		int t = in.nextInt();
-		for(int i=0;i<t;i++){
-		    String s = in.next();
-		    if (new Sol().isPossible (s) == 1)
-		        System.out.println ("Yes");
-		    else
-		        System.out.println ("No");
-		
-System.out.println("~");
-}
-	}
-}
-
-// Contributed By: Pranay Bansal
-
-
-// } Driver Code Ends
-
-
-class Sol
-{
-    int isPossible (String S)
-    {
-      int[] freq=new int[26];
+class Sol {
+    int isPossible(String S) {
+        // your code here
+        int[] freq=new int[26];
       for(char c:S.toCharArray()){
           freq[c-'a']++;
       }
