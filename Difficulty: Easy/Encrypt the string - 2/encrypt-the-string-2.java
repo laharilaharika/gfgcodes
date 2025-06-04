6879935559a89e441421719502319e12cf26c0d9
laharilaharika <155244861+laharilaharika@users.noreply.github.com>
2025-06-04 14:09:13 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Encrypt the string - 2/encrypt-the-string-2.java
+++ Difficulty: Easy/Encrypt the string - 2/encrypt-the-string-2.java
@@ -1,32 +1,3 @@
-//{ Driver Code Starts
-//Initial Template for Java
-
-import java.io.*;
-import java.util.*;
-import java.lang.*;
-
-class GFG {
-    public static void main(String args[]) throws IOException {
-        BufferedReader read =
-            new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(read.readLine());
-        while (t-- > 0) {
-            
-            String S = read.readLine();
-
-            Solution ob = new Solution();
-
-            System.out.println(ob.encryptString(S));
-        
-System.out.println("~");
-}
-    }
-}
-// } Driver Code Ends
-
-
-//User function Template for Java
-
 class Solution {
     static String encryptString(String s){
         // code here
