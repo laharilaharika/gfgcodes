68071b366615f89d13ea66090357755127eff497
laharilaharika <155244861+laharilaharika@users.noreply.github.com>
2025-06-04 13:49:50 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Check Binary String/check-binary-string.java
+++ Difficulty: Basic/Check Binary String/check-binary-string.java
@@ -1,41 +1,8 @@
-//{ Driver Code Starts
-//Initial Template for Java
+// User function Template for Java
 
-import java.io.*;
-import java.util.*;
-class GfG
-{
-    public static void main (String[] args)
-    {
-        
-        Scanner sc = new Scanner(System.in);
-        int t = sc.nextInt();
-        
-        while(t-- > 0)
-        {
-            String s = sc.next ();
-            if (new Sol().checkBinary (s))
-    		    System.out.println ("VALID");
-    		else
-    		    System.out.println ("INVALID");
-        
-System.out.println("~");
-}
-        
-    }
-}
-// Contributed By: Pranay Bansal
-
-// } Driver Code Ends
-
-
-//User function Template for Java
-
-class Sol
-{
-    Boolean checkBinary (String s)
-    {
-        // your code here     
+class Sol {
+    Boolean checkBinary(String s) {
+        // your code here
         int firstOne=-1;
         int lastOne=-1;
         for(int i=0;i<s.length();i++){
@@ -55,5 +22,4 @@ class Sol
         }
         return true;
     }
-    
 }
