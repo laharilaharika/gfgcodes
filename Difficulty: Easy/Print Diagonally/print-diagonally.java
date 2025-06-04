059ea012d58da7a74b6212b91cacdbf64740dd23
laharilaharika <155244861+laharilaharika@users.noreply.github.com>
2025-06-04 14:13:03 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Print Diagonally/print-diagonally.java
+++ Difficulty: Easy/Print Diagonally/print-diagonally.java
@@ -1,39 +1,3 @@
-//{ Driver Code Starts
-//Initial Template for Java
-
-import java.io.*;
-import java.util.*;
-class GFG
-{
-    public static void main(String args[])throws IOException
-    {
-        Scanner sc = new Scanner(System.in);
-        int t = sc.nextInt();
-        
-        while(t-- > 0)
-        {
-            int n = sc.nextInt();
-            
-            int matrix[][] = new int[n][n];
-            
-            for(int i = 0; i < n; i++)
-            {
-                for(int j = 0; j < n; j++)
-                 matrix[i][j] = sc.nextInt();
-            }
-            Solution ob = new Solution();
-            ArrayList<Integer> ans = ob.downwardDiagonal(n, matrix);
-            for (Integer val: ans) 
-                System.out.print(val+" "); 
-            System.out.println();
-        
-System.out.println("~");
-}
-    }
-}
-// } Driver Code Ends
-
-
 // User function Template for Java
 
 class Solution {
